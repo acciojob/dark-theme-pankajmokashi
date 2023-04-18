64df4e012400bd29550d6733d23e872c3b18a07e
Pankaj Mokashi <127533657+pankajmokashi@users.noreply.github.com>
2023-04-19 00:33:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
 //your code here
 
+let div = document.getElementById("app")
+let button = document.getElementById("swap")
+// console.log(div, button)
+
+function swapTheme() {
+    // console.log(div.className, div.className == "day")
+	if(div.className == "day")
+	{
+		div.className = "night"
+	}
+	else
+	{
+		div.className = "day"
+	}
+}
+button.addEventListener("click", swapTheme)
